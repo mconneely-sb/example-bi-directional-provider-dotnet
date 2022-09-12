6ee34e120ba675852c050024b5cd3969d7fe6358
mconneely-sb <84188442+mconneely-sb@users.noreply.github.com>
2022-09-12 08:49:06 +0100

    Update ProductController.cs

--- example-bi-directional-provider-dotnet/Controllers/ProductController.cs
+++ example-bi-directional-provider-dotnet/Controllers/ProductController.cs
@@ -12,10 +12,9 @@ namespace Provider.Controllers
     {
         private static List<Product> products = new List<Product>
             {
-                new Product { type = "SNACK", name = "muesli bar"},
-                new Product { type = "LUNCH", name = "sandwich"},
-                new Product { type = "DINNER", name = "burger"}
-
+                new Product { id = 2, type = "SNACK", name = "muesli bar"},
+                new Product { id = 10, type = "LUNCH", name = "sandwich"},
+                new Product { id = 27, type = "DINNER", name = "burger"}
             };
 
         // GET /products
